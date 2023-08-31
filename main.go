8209a32751a05c9b2a6f3ae195f197526b5e520f
scarfacedotcom <peteradaaku@gmail.com>
2023-08-31 22:57:19 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+func main() {
+
+}
